3b13a935acae952ad1261a4948255cd1cf1d5163
orange-maks <57869891+orange-maks@users.noreply.github.com>
2020-10-15 21:01:34 +0300

    add full pr12

--- src/pr12/Main.java
+++ src/pr12/Main.java
@@ -1,10 +1,47 @@
 package pr12;
 
-public class Main {
-    public static void main(String[] args) {
-        Planet[] planets=Planet.values();
-        for(Planet p:planets){
-            System.out.println(p.getGravity());
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame {
+
+    public Main(){
+        setTitle("Planets");
+        JButton[] buttons = new JButton[8];
+        JTextArea textArea = new JTextArea();
+        JTextArea textInfo = new JTextArea();
+        final int deltaX=180,Y=20,widthButton=150,heightButton=20;
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textInfo.setBounds(500,5,500,15);
+        textInfo.setText("Учитывайте существующую погрешность измерений и компьютера");
+        add(textInfo);
+        textInfo.setEditable(false);
+        setSize(deltaX*9,500);
+        Planet[] planets = Planet.values();
+        for(int i=0;i<buttons.length;++i) {
+            buttons[i] = new JButton(planets[i].getName());
+            buttons[i].setBounds(deltaX*i+20, Y, widthButton, heightButton);
+            add(buttons[i]);
         }
+        textArea.setBounds(500,50,500,100);
+        add(textArea);
+        textArea.setEditable(false);
+        for (int i=0;i<buttons.length;++i) {
+            int finalI = i;
+            buttons[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    String s = String.format("%s\nmas = %.0f kg\nrad = %.0f km\ngravity = %.3f m/s^-2", planets[finalI].getName(), planets[finalI].getM(), planets[finalI].getR(), planets[finalI].getGravity());
+                    textArea.setText(s);
+                }
+            });
+        }
+    }
+
+    public static void main(String[] args) {
+        new Main().setVisible(true);
     }
 }
--- src/pr12/Planet.java
+++ src/pr12/Planet.java
@@ -1,32 +1,37 @@
 package pr12;
 
 public enum Planet {
-    Mercury(3.33,23,2439),
-    Venus(4.87,24,6051),
-    Earth(5.97,24,6371),
-    Mars(6.42,23,3396),
-    Jupiter(1.8986,27,69911),
-    Saturn(5.68,26,60268),
-    Uranus(8.68,25,25360),
-    Neptune(1.02,26,24622);
+    Mercury(3.33*Math.pow(10, 23),2439000,"Mercury"),
+    Venus(4.87*Math.pow(10,24),6051000,"Venus"),
+    Earth(5.97*Math.pow(10,24),6371000,"Earth"),
+    Mars(6.42*Math.pow(10,23),3396000,"Mars"),
+    Jupiter(1.8986*Math.pow(10,27),69911000,"Jupiter"),
+    Saturn(5.68*Math.pow(10,26),60268000,"Saturn"),
+    Uranus(8.68*Math.pow(10,25),25360000,"Uranus"),
+    Neptune(1.02*Math.pow(10,26),24622000,"Neptune");
 
     private final double m,r;
-    private final int pow;
+    private final String name;
 
-    Planet(double m, int pow, double r) {
+    Planet(double m, double r, String name) {
         this.m = m;
-        this.pow = pow;
         this.r = r;
+        this.name = name;
     }
 
     public double getM() {
         return m;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public double getR() {
         return r;
     }
+
     public double getGravity(){
-        return 6.67430*this.getM()*1/(this.getR()*this.getR()*1000*1000)*Math.pow(10,this.pow-11);
+        return 6.67430*this.getM()*1/(this.getR()*this.getR())*Math.pow(10,-11);
     }
 }
